86ebc977eef16928795af88487d61d734f85db4d
MURAKI Taichi <muraki.taichi@gmail.com>
2021-11-11 14:52:24 +0900

    Fix to py3.

--- estimateDepth.py
+++ estimateDepth.py
@@ -1,9 +1,5 @@
 """This is a tensorflow tutorial that I have adapted for the CNN part of our project!"""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import tensorflow as tf
 import numpy as np
 
